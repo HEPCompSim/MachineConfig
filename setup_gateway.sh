07e01bd6dd3bc504786069e023ef1708324af798
RHofsaess <Robin.Hofsaess@kit.edu>
2022-05-17 14:16:57 +0200

    Scripts updated. Raid setup added.

--- setup_gateway.sh
+++ setup_gateway.sh
@@ -9,7 +9,7 @@ systemctl restart network
 # Add ip
 ip addr add 192.168.1.100/24 dev enp4s0 label enp4s0:0
 # Enable routing
-iptables -t nat -A POSTROUTING -o enp4s0 -j SNAT --to 129.13.101.148 #SNAT
+iptables -t nat -A POSTROUTING -o enp4s0 -j SNAT --to 129.13.101.146 #SNAT
 
 # For testing:
 iptables -t nat -nvL # SNAT
--- setup_worker.sh
+++ setup_worker.sh
@@ -8,13 +8,16 @@ systemctl restart network
 ### Add local ip address:
 if [ -z "$1" ]
 then
-	        echo "usage: ./setup_worker.sh <IP's last digit>"
-		        exit -1
+	                echo "usage: ./setup_worker.sh <IP's last digit>"
+			                        exit -1
 fi
 ip addr add 192.168.1.10$1/24 dev enp4s0 label enp4s0:0
 
 # add default gateway:
 route add default gw 192.168.1.100 enp4s0
+
+# mount condor
+mount /dev/md0 /local/scratch/condor
 
 #For testing:
 echo "gateway:"
