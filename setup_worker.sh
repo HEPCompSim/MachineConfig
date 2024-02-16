ed18cf91c1195725f5321d965a4922f14558fe9f
Maximilian Horzela <33454678+HerrHorizontal@users.noreply.github.com>
2024-02-16 10:36:36 +0100

    Update worker setup for Ubutu 22.04

--- setup_worker.sh
+++ setup_worker.sh
@@ -1,23 +1,27 @@
-#deactivate ip v6 in /etc/sysctl.conf on all machines
-echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
-
-sysctl -p  # apply
-systemctl restart network
-
-### Add local ip address:
 if [ -z "$1" ]
 then
-	                echo "usage: ./setup_worker.sh <IP's last digit>"
-			                        exit -1
+                        echo "usage: ./setup_worker.sh <IP's last digit>"
+                                                exit -1
 fi
+
+#deactivate ip v6 in /etc/sysctl.conf on all machines
+#echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+#echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+#sysctl -p  # apply
+#systemctl restart systemd-networkd
+
+### Add local ip address:
 ip addr add 192.168.1.10$1/24 dev enp4s0 label enp4s0:0
 
 # add default gateway:
-route add default gw 192.168.1.100 enp4s0
+route replace default gw 192.168.1.100 enp4s0
+# remove old default gateway:
+#route del default gw 129.13.101.1 dev enp4s0
 
 # mount condor
-mount /dev/md0 /local/scratch/condor
+#mount /dev/md0 /local/scratch/condor
 
 #For testing:
 echo "gateway:"
